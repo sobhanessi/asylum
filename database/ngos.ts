e5544a4d82977875bfda1931da0220f1b2a1357a
sobhanessi <sobhan@esfandyari.org>
2022-09-29 11:29:54 +0200

    fixed the issue with rowing

--- database/ngos.ts
+++ database/ngos.ts
@@ -1,19 +1,84 @@
 interface ngos {
+  key: string;
   name: string;
   logo: string;
-  services: object;
-  information: string;
-  address: string;
-  telephones?: object;
+  services?: string[];
+  information?: string;
+  address?: string;
+  telephones?: string[];
 }
 
-export const ngos: [ngos] = [
+const ngos: ngos[] = [
   {
-    name: "",
-    logo: "",
-    services: [],
-    information: "",
-    address: "",
-    telephones: [],
+    key: "1",
+    name: "made up 1",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
+  },
+  {
+    key: "2",
+    name: "made up 2",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
+  },
+  {
+    key: "3",
+    name: "made up 3",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
+  },
+  {
+    key: "4",
+    name: "made up 4",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
+  },
+  {
+    key: "5",
+    name: "made up 5",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
+  },
+  {
+    key: "6",
+    name: "made up 6",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
+  },
+  {
+    key: "7",
+    name: "made up 7",
+    logo: "logo.png",
+    services: ["Psycho social", "Sports", "Lawyer"],
+    information:
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+    address: "Mochengladbach, 41179, EAE JHQ",
+    telephones: ["004915123516042"],
   },
 ];
+
+export default ngos;
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,65 @@
-import { StyleSheet, Text, View } from "react-native";
+import ngos from "../database/ngos";
 import React from "react";
+import {
+  FlatList,
+  Image,
+  // ListViewBase,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-const Home = () => {
+interface ngo {
+  key: string;
+  name: string;
+  logo: string;
+  services?: string[];
+  information?: string;
+  address?: string;
+  telephones?: string[];
+}
+
+const Item = ({ item }: { item: ngo }) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      <Image
+        source={require("../assets/logo.png")}
+        style={{
+          // display: "flex",
+          width: 120,
+          height: 120,
+        }}
+      />
+      <Text>{item.name}</Text>
+      <Text>{}</Text>
     </View>
+  );
+};
+const Home = () => {
+  const renderItem = ({ item }: { item: ngo }) => <Item item={item} />;
+
+  return (
+    <ScrollView
+      style={{
+        flex: 1,
+        width: "100%",
+        // flexWrap: "wrap",
+      }}
+      horizontal={true}
+    >
+      <FlatList
+        data={ngos}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.key}
+        numColumns={2}
+      />
+    </ScrollView>
   );
 };
 
