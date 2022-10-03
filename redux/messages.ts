a146b1e061eeff1a4aebb99461705838281dfaee
sobhanessi <sobhan@esfandyari.org>
2022-10-03 09:10:50 +0200

    experimenting on the redux tools

--- redux/messages.ts
+++ redux/messages.ts
@@ -6,6 +6,10 @@ export const messages = createSlice({
     message: "",
   },
   reducers: {
+    numberOfMessages: (state, action) => {
+      // number of received messages
+      // here I also must reduce numbers regarding reading the messages.
+    },
     receive: (state, action) => {
       // state.message = action.payload.message;
     },
