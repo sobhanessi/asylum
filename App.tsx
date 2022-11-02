4b4e9e5261320114d7728e60c7090d74bfdce9ec
sobhanessi <sobhan@esfandyari.org>
2022-11-02 18:56:12 +0100

    nothing but just a todo

--- App.tsx
+++ App.tsx
@@ -8,6 +8,12 @@ import { StyleSheet } from "react-native";
 import React from "react";
 
 export default function App() {
+  // 1. inja bayad user ro az storage begiram va baadesh loadesh konam...
+  // agar ok bood ke hich amma agar na ke bayad dobare login kone.
+  // 2. aya be redux ehtiaj daram ? fekr konam na.
+  // 3. dar marhaleye konooni faghat rooye ye app e static tamarkoz konam.
+  // 4. ghabeliate search ro faraham konam ke user ha betoonan un chizi...
+  // ke mikhan ro peyda konan.
   return (
     // <Provider store={store}>
     <SafeAreaView style={styles.container}>
